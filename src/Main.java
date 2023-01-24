1f30d69960727a7341fa9a715cce0f88df016034
Eugen <ezdiviner@gmail.com>
2023-01-24 11:16:01 +0200

    new method move

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,40 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main extends JPanel {
+
+    final int BF_WIDTH = 576;
+    final int BF_HEIGHT = 576;
+
     //1 - Up, 2 - Down, 3 - Left, 4 - Right
-    int direction = 4;
+    int direction = 1;
 
     int x = 256;
     int y = 256;
 
-    public static void main(String[] args) {
+    void moveUp() throws Exception {
+        direction = 1;
+        while (y != 0 ){
+            y--;
+            Thread.sleep(33);
+            repaint();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+
+        main.moveUp();
+    }
+
+    Main(){
         JFrame frame = new JFrame("Dandy Tanks");
-        frame.setMinimumSize(new Dimension(576,576));
-        frame.getContentPane().add(new Main());
+        frame.setMinimumSize(new Dimension(BF_WIDTH,BF_HEIGHT));
+        frame.getContentPane().add(this);
         frame.setLocation(0,0);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
+
     //Tank
     @Override
     protected void paintComponent(Graphics g){
